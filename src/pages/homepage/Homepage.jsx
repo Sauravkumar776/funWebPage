00d8c62051efffbed6497bc9a70e8012d76d136d
sauravkumar776 <ashukumar752861@gmail.com>
2022-09-15 14:31:16 +0530

    Adds Homepage component.

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/navbar/NavBar';
+import OrderTrackingCard from '../../components/orderTracking/OrderTrackingCard';
+
+const Homepage = () => {
+  return (
+    <>
+        <NavBar />
+        <OrderTrackingCard />
+    </>
+  )
+}
+
+export default Homepage
